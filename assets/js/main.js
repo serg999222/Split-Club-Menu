6ca32c87d25b30dcb4b501b9c8b24c7559b95292
serg999222 <gazenkos935@gmail.com>
2024-01-08 13:41:11 +0200

    stiky footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,1 @@
-window.addEventListener('load', function () {
-	// Визначте відстань для прокрутки
-	var scrollDistance = window.innerHeight; // може бути потрібно дещо корегувати
 
-	// Прокрутіть сторінку на визначену відстань
-	window.scroll(0, -scrollDistance);
-});
